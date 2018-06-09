3922b1e5898dffd56a0b37899a9e6908a1e20f0b
soarflat <soarflat777@gmail.com>
2018-06-09 16:50:46 +0900

    wip

---
+++ func/random.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const qs = require('qs');
+
+export function handler(event, context, callback) {
+  const { API_URL, API_ID, AFFILIATE_ID, SITE } = process.env;
+  const API_PARAMS = qs.stringify(event.queryStringParameters);
+  const URL = `${API_URL}ItemList?${API_PARAMS}&api_id=${API_ID}&affiliate_id=${AFFILIATE_ID}&site=${SITE}`;
+
+  const respond = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ x: 5, y: 6 })
+    });
+  };
+
+  const get = () => {
+    axios
+      .get(URL)
+      .then(response => {
+        console.log(response.data);
+        console.log(response.data.result.items[0]);
+        respond(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+        respond(err);
+      });
+  };
+
+  if (event.httpMethod == 'GET') {
+    get();
+  } else {
+    callback(null, {
+      statusCode: 410,
+      body: 'Unsupported Request Method'
+    });
+  }
+}
